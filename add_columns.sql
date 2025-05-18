7648ad9819d2b8e24254bb5413bbb5d7c1455a73
charlesleooo <charlesleohermano@gmail.com>
2025-05-18 20:40:53 +0800

    latest database

--- add_columns.sql
+++
@@ -1,7 +0,0 @@
--- Add password and is_temp_password columns to employees table
-ALTER TABLE employees 
-ADD COLUMN password VARCHAR(255) DEFAULT NULL,
-ADD COLUMN is_temp_password TINYINT(1) NOT NULL DEFAULT 1;
-
--- Update existing employees to require password change
-UPDATE employees SET is_temp_password = 1;
