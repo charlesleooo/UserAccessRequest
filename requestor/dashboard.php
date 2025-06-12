5ce480de8fe298fae63f227e80e6a37fd3ce9c0c
alvinthemaster <alvintampus3@gmail.com>
2025-06-12 11:58:13 +0800

    fixed request table

--- requestor/dashboard.php
+++ requestor/dashboard.php
@@ -543,6 +543,9 @@ try {
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             UAR REF NO.
                                         </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Status
+                                        </th>
                                         
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Date Requested
