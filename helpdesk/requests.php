a0263ff7614f1da5269c350bd2100b327325efcd
Charles <charlesleohermano@gmail.com>
2025-09-24 08:11:38 +0800

    added user_management to my helpdesk

--- helpdesk/requests.php
+++ helpdesk/requests.php
@@ -142,6 +142,14 @@ try {
                         </span>
                         <span class="ml-3">Review History</span>
                     </a>
+
+                    <a href="user_management.php" class="flex items-center px-4 py-3 text-gray-700 rounded-xl transition-all hover:bg-gray-50 hover:text-primary-600 group">
+                        <span class="flex items-center justify-center w-9 h-9 bg-gray-100 text-gray-600 rounded-lg group-hover:bg-primary-50 group-hover:text-primary-600">
+                            <i class='bx bx-user text-xl'></i>
+                        </span>
+                        <span class="ml-3">User Management</span>
+                    </a>
+                    
                     <a href="settings.php" class="flex items-center px-4 py-3 text-gray-700 rounded-xl hover:bg-gray-50">
                         <span class="flex items-center justify-center w-9 h-9 bg-gray-100 text-gray-600 rounded-lg">
                             <i class='bx bx-cog text-xl'></i>
